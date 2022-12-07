d930f31f8f2230db2f06a4d3df6dfc0f713d0c53
Nganthoiba Ayekpam <116248082+Nganthoibayekpam@users.noreply.github.com>
2022-12-07 14:41:36 +0530

    Update practicals.py

--- practicals.py
+++ practicals.py
@@ -245,3 +245,48 @@ def disp_rec():
             print("Search successful! :)")
         fil.close()
 disp_rec()
+
+#14 A binary file “Store.dat” has structure [ItemNo, Item_Name, Company, Price].
+#i) Write a function CountRec(Company) in Python which accepts the Company name as parameter and count and return number of Items by the given Company are 
+#stored in the binary file “Store.dat”.
+#ii) Write a function AddRecord(<List>) which accepts a List of the record [ItemNo, Item_Name, Company, Price] and appends in the binary file “Store.Dat”
+import pickle
+
+def write_file():
+	f = open("store.dat", "wb")
+	op = 'y'
+	while op == 'y':
+		ItemNo = int(input("enter item number: "))
+		Item_Name = input("Enter item name: ")
+		Company = input("Enter company name: ")
+		Price = int(input("Enter price: "))
+		pickle.dump([ItemNo, Item_Name, Company, Price], f)
+		op = input("Do you want to continue? (y or n)")
+	f.close()
+
+print("entering company details..")
+write_file()
+
+def CountRec(company):
+    f=open("store.dat","rb")
+    p=[]
+    h={}
+    try:
+        while True:
+            g=pickle.load(f)
+            p.append(g)
+    except EOFError:
+        for i in p:
+            if i[2] in h:
+                h[i[2]]=h[i[2]]+1
+            else:
+                h[i[2]]=1
+        n=h.items()
+        for i in n:
+            if i[0]==company:
+                print('Company name:',i[0],"Count ",i[1])
+    f.close()
+def AddRecord(list):
+    fh=open("store.dat","wb")
+    g=pickle.dump(list,f)
+    fh.close()
